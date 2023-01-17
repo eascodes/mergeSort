266af8069124022ddbdac1b812a20c5c9746dab3
Emily <easchoettler@gmail.com>
2023-01-16 19:30:12 -0500

    Add mergeSort and merge functions

---
+++ main.js
@@ -0,0 +1,33 @@
+function mergeSort(arr) {
+    if (arr.length < 2) {
+        return arr;
+    };
+    
+    let left = arr.slice(0,((arr.length/2)));
+    let right = arr.slice((arr.length/2),arr.length);
+    
+    left = mergeSort(left);
+    right = mergeSort(right);
+    
+    return merge(left, right);    
+}
+
+function merge(arr1, arr2) {
+    let result = [];
+
+    if (arr1 == undefined) {
+        return arr2;
+    } else if (arr2 == undefined) {
+        return arr1;
+    }
+
+    let i=0, j=0, k=0;
+    while (k < (arr1.length + arr2.length)) {
+        if (arr1[i] < arr2[j] || arr2[j] == undefined) {
+            result[k++] = arr1[i++]
+        } else if (arr2[j] < arr1[i] || arr1[i] == undefined) {
+            result[k++] = arr2[j++]
+        }
+    }
+    return result;
+}
