e4c737945daf136feb2cc8c41ea9a44fa076c9c0
Emily <easchoettler@gmail.com>
2023-01-17 09:17:40 -0500

    Fix indentation

--- main.js
+++ main.js
@@ -10,23 +10,23 @@ function mergeSort(arr) {
     right = mergeSort(right);
 
     function merge(arr1, arr2) {
-    let result = [];
+        let result = [];
 
-    if (arr1 == undefined) {
-        return arr2;
-    } else if (arr2 == undefined) {
-        return arr1;
-    }
-
-    let i=0, j=0, k=0;
-    while (k < (arr1.length + arr2.length)) {
-        if (arr1[i] < arr2[j] || arr2[j] == undefined) {
-            result[k++] = arr1[i++]
-        } else if (arr2[j] < arr1[i] || arr1[i] == undefined) {
-            result[k++] = arr2[j++]
+        if (arr1 == undefined) {
+            return arr2;
+        } else if (arr2 == undefined) {
+            return arr1;
         }
-    }
-    return result;
+
+        let i=0, j=0, k=0;
+        while (k < (arr1.length + arr2.length)) {
+            if (arr1[i] < arr2[j] || arr2[j] == undefined) {
+                result[k++] = arr1[i++]
+            } else if (arr2[j] < arr1[i] || arr1[i] == undefined) {
+                result[k++] = arr2[j++]
+            }
+        }
+        return result;
     }  
     
     return merge(left, right);    
